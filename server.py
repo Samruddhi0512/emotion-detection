423c8359e417856b7a906e4506a53154050d3ae0
Samruddhi Sunil Bate <samruddhibate05@gmail.com>
2026-09-18 15:20:05 +0000

    Add Flask server

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+
+@app.route("/emotionDetector")
+def emotion_detector_handler():
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    result = emotion_detector(text_to_analyze)
+
+    formatted_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+    return jsonify({"formatted_response": formatted_response})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
